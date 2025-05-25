2960d9d660929307b1d0c1756197ba818b0945b8
Lê Khải Hoàn <129830151+yuu-erp@users.noreply.github.com>
2025-05-25 22:59:12 +0700

    chore: update scraper cli 🐳

--- src/cli/commands/scraper.ts
+++ src/cli/commands/scraper.ts
@@ -1,51 +1,53 @@
 import { select, Separator } from '@inquirer/prompts'
 import { Command } from 'commander'
-import scrapers from '~/scrapers'
+import scrapers, { getScraper, ScraperType } from '~/scrapers'
 
 export default (program: Command) => {
   return program
     .command('scraper')
     .description('Scrape a manga')
     .action(async () => {
-      const scraperType = await select({
+      const scraperType: ScraperType = await select({
         message: 'Please select the type of media you want to scrape:',
         choices: [
-            {
-                name: 'Manga',
-                value: 'manga',
-                description: 'Scrape a manga from a website'
-            },
-            new Separator(),
-            {
-                name: 'Anime',
-                value: 'anime',
-                disabled: true,
-                description: 'Scrape a anime from a website'
-            },
-            {
-                name: 'Light Novel',
-                value: 'light-novel',
-                disabled: true,
-                description: 'Scrape a light novel from a website'
-            }
+          {
+            name: 'Manga',
+            value: 'manga',
+            description: 'Scrape a manga from a website'
+          },
+          new Separator(),
+          {
+            name: 'Anime',
+            value: 'anime',
+            disabled: true,
+            description: 'Scrape a anime from a website'
+          },
+          {
+            name: 'Light Novel',
+            value: 'lightNovel',
+            disabled: true,
+            description: 'Scrape a light novel from a website'
+          }
         ]
       })
       const scraperID = await select({
         message: 'Please select the scraper you want to use:',
         choices: [
-            ...Object.keys(scrapers.manga).map((scraper) => ({ 
-                name: scrapers.manga[scraper].name,
-                value: scraper,
-                description: `Scrape ${scrapers.manga[scraper].name}`
-            })),
-            new Separator(),
-            {
-                name: 'Custom',
-                value: 'custom',
-                description: 'Scrape a custom website'
-            }
+          ...Object.keys(scrapers[scraperType]).map((scraper) => ({
+            name: scrapers[scraperType][scraper].name,
+            value: scraper,
+            description: `Scrape ${scrapers[scraperType][scraper].name}`
+          })),
+          new Separator(),
+          {
+            name: 'Custom',
+            value: 'custom',
+            disabled: true,
+            description: 'Scrape a custom website'
+          }
         ]
-      })    
-      console.log(scraperType, scraperID)
+      })
+      const scraper = getScraper(scraperID)
+      console.log('Pushing scraper info to database');
     })
 }
--- src/scrapers/index.ts
+++ src/scrapers/index.ts
@@ -3,14 +3,18 @@ import { ManageScraper } from '~/core/manage-scraper'
 import handlePath from '~/utils/handle-file'
 
 export type MangaScraperId = string
+export type AnimeScraperId = string
+export type LightNovelScraperId = string
 
-const readScrapers = (path: string) => {
+export type ScraperType = 'manga' | 'anime' | 'lightNovel'
+
+const readScrapers = (path: string): Record<MangaScraperId | AnimeScraperId | LightNovelScraperId, ManageScraper> => {
   const scraperFiles = fs
     .readdirSync(handlePath(path))
     .filter((file) => file.endsWith('.ts'))
     .map((file) => file.replace('.ts', ''))
 
-  const scrapers: Record<MangaScraperId, ManageScraper> = {}
+  const scrapers: Record<MangaScraperId | AnimeScraperId | LightNovelScraperId, ManageScraper> = {}
 
   for (const file of scraperFiles) {
     const { default: Scraper } = require(handlePath(`${path}/${file}.ts`))
@@ -20,5 +24,35 @@ const readScrapers = (path: string) => {
 }
 
 const mangaScrapers: Record<MangaScraperId, ManageScraper> = readScrapers('./scrapers/manga')
+const animeScrapers: Record<AnimeScraperId, ManageScraper> = readScrapers('./scrapers/anime')
+const lightNovelScrapers: Record<LightNovelScraperId, ManageScraper> = readScrapers('./scrapers/lightNovel')
 
-export default { manga: mangaScrapers }
+const getAnimeScraper = (id: AnimeScraperId) => {
+  return animeScrapers[id]
+}
+
+const getMangaScraper = (id: MangaScraperId) => {
+  return mangaScrapers[id]
+}
+
+const getLightNovelScraper = (id: LightNovelScraperId) => {
+  return lightNovelScrapers[id]
+}
+
+export const getScraper = (id: AnimeScraperId | MangaScraperId | LightNovelScraperId) => {
+  if (id in animeScrapers) {
+    return getAnimeScraper(id)
+  }
+
+  if (id in mangaScrapers) {
+    return getMangaScraper(id)
+  }
+
+  if (id in lightNovelScrapers) {
+    return getLightNovelScraper(id)
+  }
+
+  throw new Error(`Unknown scraper id: ${id}`)
+}
+
+export default { manga: mangaScrapers, anime: animeScrapers, lightNovel: lightNovelScrapers }
